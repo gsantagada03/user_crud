02c4769bb67d5acf0e2c673db4f9d1d6d290ce11
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-04-10 14:59:00 +0200

    Delete group
    Metodo per cancellare un gruppo
    
    Mattia 14:58 10/04

--- src/main/java/com/service/GroupDAO.java
+++ src/main/java/com/service/GroupDAO.java
@@ -8,18 +8,17 @@ import java.sql.SQLException;
 import com.models.Group;
 
 public class GroupDAO {
-    
+
     private String url = "jdbc:postgresql://localhost:5432/crud";
     private String user = "postgres";
     private String password = "root";
 
-
-    //metodo per aggiornare un gruppo
+    // metodo per aggiornare un gruppo
     public void updateGroup(Group group) {
         String query = "UPDATE groups SET name = ?, password = ?, isprivate = ? WHERE id = ?";
 
         try (Connection conn = DriverManager.getConnection(url, this.user, this.password);
-        PreparedStatement pstmt = conn.prepareStatement(query)) {
+                PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setString(1, group.getName());
             pstmt.setString(2, group.getPassword());
             pstmt.setBoolean(3, group.isPrivate());
@@ -31,10 +30,19 @@ public class GroupDAO {
         }
     }
 
-
-    //metodo per cancellare un gruppo
+    // metodo per cancellare un gruppo
 
     public void deleteGroup(Group group) {
         String query = "DELETE from groups WHERE id = ?";
+
+        try (Connection conn = DriverManager.getConnection(url, this.user, this.password);
+                PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setInt(1, group.getId());
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
